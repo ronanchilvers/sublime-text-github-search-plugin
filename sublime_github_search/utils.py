fcfbaf10673a3392b534f811c4d7606f18b5c298
Ronan Chilvers <ronan@d3r.com>
2023-03-16 09:36:23 +0000

    Avoid double encoding

--- sublime_github_search/utils.py
+++ sublime_github_search/utils.py
@@ -24,11 +24,11 @@ def open_search_tab(text, search_type):
         "type=" + search_type
     ]
     search_term = [
-        encode(text)
+        text
     ]
     defaults = get_setting("default_options", {})
     for key,val in defaults.items():
         search_term.append(key + ":" + val)
     url = get_setting("base_url", "https://github.com/search")
-    qs.append("q=" + encode(" ".join(search_term)))
+    qs.append("q=" + "+".join(search_term))
     webbrowser.open_new_tab( url + "?" + "&".join(qs) )
